3a8e6bf22163ffd2cf9d0c268b811f42a61f033e
Maurice Bos <m-ou.se@m-ou.se>
2016-10-12 17:19:14 +0200

    Update error printing.

--- src/print_error.cpp
+++ src/print_error.cpp
@@ -8,25 +8,13 @@
 
 namespace conftaal {
 
-void print_message(string_view message, string_tracker const & tracker, string_view where, bool color, bool error) {
-	auto bad_src = tracker.get(where);
-	auto bad_src_end = tracker.get(where.data() + where.size());
-	if (color) std::clog << "\033[1m";
-	std::clog << bad_src.location << ": ";
-	if (error) {
-		if (color) std::clog << "\033[31m";
-		std::clog << "error: ";
-		if (color) std::clog << "\033[;1m";
-	}
-	std::clog << message;
-	if (color) std::clog << "\033[m";
-	std::clog << std::endl;
-	if (bad_src.location.column == 0 || bad_src.original_char - &bad_src.original_source[0] >= bad_src.location.column - 1) {
-		char const * bad_line_start = bad_src.original_char - (bad_src.location.column - 1);
+void print_source_snippet(string_tracker::get_result const & src, string_tracker::get_result const & src_end, bool color) {
+	if (src.location.column == 0 || src.original_char - &src.original_source[0] >= src.location.column - 1) {
+		char const * bad_line_start = src.original_char - (src.location.column - 1);
 		std::string pointer_line;
-		char const * original_source_end = bad_src.original_source.data() + bad_src.original_source.size();
+		char const * original_source_end = src.original_source.data() + src.original_source.size();
 		for (char const * c = bad_line_start; ; ++c) {
-			if (c == bad_src.original_char || (c > bad_src.original_char && c < bad_src_end.original_char)) {
+			if (c == src.original_char || (c > src.original_char && c < src_end.original_char)) {
 				pointer_line += '^';
 			} else {
 				pointer_line += ' ';
@@ -56,9 +44,28 @@ void print_message(string_view message, string_tracker const & tracker, string_v
 	}
 }
 
-void print_error(ParseError const & error, string_tracker const & tracker, bool color) {
-	print_message(error.what(), tracker, error.where(), color, true);
-	for (auto const & note : error.notes()) {
+void print_message(string_view message, string_tracker const & tracker, string_view where, bool color, bool error) {
+	auto bad_src = tracker.get(where);
+	if (color) std::clog << "\033[1m";
+	if (bad_src.location) std::clog << bad_src.location << ": ";
+	if (error) {
+		if (color) std::clog << "\033[31m";
+		std::clog << "error: ";
+		if (color) std::clog << "\033[;1m";
+	}
+	std::clog << message;
+	if (color) std::clog << "\033[m";
+	std::clog << std::endl;
+
+	if (bad_src.location) {
+		auto bad_src_end = tracker.get(where.data() + where.size());
+		print_source_snippet(bad_src, bad_src_end, color);
+	}
+}
+
+void print_error(Error const & error, string_tracker const & tracker, bool color) {
+	print_message(error.what(), tracker, error.where, color, true);
+	for (auto const & note : error.notes) {
 		print_message(note.first, tracker, note.second, color, false);
 	}
 }
--- src/print_error.hpp
+++ src/print_error.hpp
@@ -1,12 +1,12 @@
 #include <string_view.hpp>
 #include <string_tracker.hpp>
 
-#include "parse.hpp"
+#include "error.hpp"
 
 namespace conftaal {
 
 void print_message(string_view message, string_tracker const &, string_view where, bool color = true, bool error = false);
 
-void print_error(ParseError const &, string_tracker const &, bool color = true);
+void print_error(Error const &, string_tracker const &, bool color = true);
 
 }
